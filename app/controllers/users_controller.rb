8c0a9bb7f74fa2f53fc6fe41622b76774d29a03a
Curtis Chadwell <laurencechadwell@gmail.com>
2020-06-16 00:25:34 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,15 +40,16 @@ class UsersController < ApplicationController
         
       end
       
-      # @user = User.find_by(:username => params[:username], password: params[:password])
-      #   if @user
-      #       session[:user_id] = @user.id
-      #     redirect to "/tweets"
-      #   else
-      #     redirect to '/signup'
-      #   end
-        
-    end
+  end
+  
+      get '/logout' do
+        if logged_in?
+          session.destroy
+          redirect to '/login'
+        else
+          redirect '/'
+        end
+      end
 
 
     
