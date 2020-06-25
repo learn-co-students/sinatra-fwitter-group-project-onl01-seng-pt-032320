8b8239601e7090ea62a9f8c98a5b3955eb0f525a
Abdul Khan <akhan17@outlook.com>
2020-06-25 21:44:55 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,15 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    binding.pry
+    if session[:User_id] != nil
+      @user = User.find_by_id(session[:User_id])
+      @tweets = Tweet.all
+      binding.pry
+      erb :'/tweets/tweets'
+    else
+      redirect 'login'
+    end
+  end
 
 end
