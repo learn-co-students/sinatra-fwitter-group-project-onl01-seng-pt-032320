517cfdf3011707b589c31611b331beb6165f218a
github username <github email address>
2020-06-26 13:22:38 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
 
-
+    get '/tweets' do
+        if !Helpers.logged_in?(session)
+            redirect to '/login'
+        end
+        @tweets = Tweet.all
+        @user = Helpers.current_user(session)
+        erb :'tweets/tweets'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,56 @@
 class UsersController < ApplicationController
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
 
+  get '/signup' do
+    if !Helpers.logged_in?(session)
+      erb :'users/create_user'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      if user.save
+        session[:user_id] = user.id
+        redirect to '/tweets'
+      end
+    end
+  end
+
+  get '/login' do
+    if Helpers.logged_in?(session)
+      redirect to '/tweets'
+    end
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    if Helpers.logged_in?(session)
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 
 end
+
+  
