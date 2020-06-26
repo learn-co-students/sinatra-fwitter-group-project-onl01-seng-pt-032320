90cecec272bc2b93685287ae318ec77e079d5b92
Abdul Khan <akhan17@outlook.com>
2020-06-26 22:47:50 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,7 +17,7 @@ class TweetsController < ApplicationController
   erb :"/tweets/create_tweet"
 end
 
-post '/tweets' do
+  post '/tweets' do
     user = Helpers.current_user(session)
     if params["content"].empty?
       redirect to '/tweets/new'
@@ -26,5 +26,35 @@ post '/tweets' do
 
     redirect to '/tweets'
   end
+
+  get '/tweets/:id' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find(params[:id])
+    erb :"tweets/show_tweet"
+  end
+
+  get '/tweets/:id/edit' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find(params[:id])
+    if Helpers.current_user(session).id != @tweet.user_id
+      redirect to '/tweets'
+    end
+    erb :"tweets/edit_tweet"
+  end
+
+  patch '/tweets/:id' do
+  tweet = Tweet.find(params[:id])
+  if params["content"].empty?
+    redirect to "/tweets/#{params[:id]}/edit"
+  end
+  tweet.update(:content => params["content"])
+  tweet.save
+
+  redirect to "/tweets/#{tweet.id}"
+end
 
 end
