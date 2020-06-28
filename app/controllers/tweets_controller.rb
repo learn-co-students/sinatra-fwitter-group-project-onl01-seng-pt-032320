093fedc43c93799a196931e782ae588b7dda3375
Allan5829 <allan.farca@gmail.com>
2020-06-28 02:57:58 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
 
+    get "/tweets" do
+        if Helpers.is_logged_in?(session)
+            erb :'/tweets/tweets'
+        else
+            redirect "/login"
+        end
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,71 @@
+require 'pry'
 class UsersController < ApplicationController
 
+    get "/" do
+        if Helpers.is_logged_in?(session)
+            redirect "/tweets"
+        else
+            erb :'/users/index'
+        end
+    end
+
+    get "/signup" do
+        if Helpers.is_logged_in?(session)
+            redirect "/tweets"
+        end 
+        erb :'/users/signup'
+    end
+
+    post "/signup" do
+        if params[:username] == "" || 
+            params[:email] == "" || 
+            params[:password] == ""
+            redirect "/signup"
+        end 
+      
+        user = User.new(
+            :username => params[:username], 
+            :email => params[:email],
+            :password => params[:password]) 
+      
+        if user.save
+            session[:user_id] = user.id
+			redirect "/tweets"
+        else
+            redirect "/signup"
+        end
+    end 
+
+    get "/login" do
+        if Helpers.is_logged_in?(session)
+            redirect "/tweets"
+        else
+            erb :'/users/login'
+        end
+    end
+
+    post "/login" do 
+        user = User.find_by(:username => params[:username])
+ 
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/tweets"
+		else
+			redirect "/login"
+		end
+    end 
+
+    get "/failure" do
+        erb :'/users/failure'
+    end
+
+    get "/logout" do
+        if Helpers.is_logged_in?(session)
+            session.clear
+            redirect "/login"
+        else
+            redirect "/"
+        end
+    end
 
 end
