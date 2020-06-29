ba6cad58da1ecf88064e3803ec4589555f4755fc
Allan5829 <allan.farca@gmail.com>
2020-06-29 02:58:37 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -40,9 +40,45 @@ class TweetsController < ApplicationController
     end 
 
     get "/tweets/:id/edit" do 
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find_by_id(params[:id])
+            @user = Helpers.current_user(session)
+            if @tweet.user_id == @user.id 
+                erb :'/tweets/edit'
+            else
+                redirect "/tweets/#{@tweet.id}"
+            end 
+        else
+            redirect "/login"
+        end
+    end 
+
+    post "/tweets/:id" do 
+        binding.pry
+        if params[:content] == ""
+            binding.pry
+            redirect "/tweets/#{@tweet.id}/edit"
+        end 
+
+        @tweet = Tweet.find_by_id(params[:id])
+        @tweet.content = params[:content]
+        @tweet.save
+        redirect to "/tweets/#{@tweet.id}"
     end 
 
     delete "/tweets/:id/delete" do 
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find_by_id(params[:id])
+            @user = Helpers.current_user(session)
+            if @tweet.user_id == @user.id 
+                @tweet.delete
+                redirect "/tweets"
+            else
+                redirect "/tweets/#{@tweet.id}"
+            end 
+        else
+            redirect "/login"
+        end
     end 
 
 end
