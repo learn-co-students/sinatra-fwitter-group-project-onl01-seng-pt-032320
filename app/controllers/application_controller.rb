ce3841dd80296e2c3e8a7a8a42e1925a6e2078a0
sergezolo <zolotukhinsereg@gmail.com>
2020-06-30 21:30:23 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "FBI knows everything"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?		
+      !!session[:id]	
+    end		
+        
+    def current_user		
+      @user ||= User.find_by_id(session[:id]) if logged_in?	
+    end	
+    
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+            redirect "/tweets"
+        else
+            erb :'/users/create_user'
+        end        
+    end
+    
+    post '/signup' do
+        user = User.create(params)
+        session[:id] = user.id
+        redirect '/tweets'
+    end
+    
+    get '/login' do
+        if logged_in?
+            redirect "/tweets"
+        else
+            erb :'/users/login'
+        end        
+    end
+    
+    post '/login' do
+        user = User.find_by(username: [:username])
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect '/tweets'
+        else
+            redirect '/users/login'
+        end
+    end
+    
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
 
 end
