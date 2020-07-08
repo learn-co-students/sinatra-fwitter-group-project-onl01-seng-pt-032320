b6700c1cb55bf8b2873db0e777959c3fcf93f116
Caroline <cvrieous@gmail.com>
2020-07-08 10:50:15 -0500

    Complete the lab

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,60 @@
+# frozen_string_literal: true
+
 class TweetsController < ApplicationController
+  get '/tweets', auth: nil do
+    @user = current_user
+    @tweets = Tweet.all
 
+    erb :'/tweets/index'
+  end
 
+  post '/tweets', auth: nil do
+    tweet = Tweet.create(content: params[:content])
+
+    if tweet.valid?
+      tweet.user = current_user
+      tweet.save
+
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/new', auth: nil do
+    erb :'/tweets/new'
+  end
+
+  get '/tweets/:id', auth: nil do
+    @tweet = Tweet.find(params[:id])
+
+    erb :'/tweets/show'
+  end
+
+  patch '/tweets/:id', auth: nil do
+    tweet = Tweet.find(params[:id])
+    tweet.update(content: params[:content])
+
+    if tweet.valid?
+      tweet.save
+
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id', auth: nil do
+    tweet = Tweet.find(params[:id])
+
+    tweet.destroy if tweet.user == current_user
+
+    redirect '/tweets'
+  end
+
+  get '/tweets/:id/edit', auth: nil do
+    @tweet = Tweet.find(params[:id])
+
+    erb :'/tweets/edit'
+  end
 end
---
+++ db/migrate/20200707021641_create_tweet.rb
@@ -0,0 +1,8 @@
+class CreateTweet < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.string :content
+      t.timestamps
+    end
+  end
+end
