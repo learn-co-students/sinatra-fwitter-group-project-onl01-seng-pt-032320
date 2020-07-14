5076db431e46c9d23ec262a70043ab2c969c40b4
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-07-14 19:11:49 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2020_07_14_165245) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.string "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
 end
