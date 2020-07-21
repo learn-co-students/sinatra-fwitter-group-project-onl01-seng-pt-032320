e5fffcca82f315e545c0a545d4caecdfa281c355
Bryce B <bryce.boddy@gmail.com>
2020-07-21 10:27:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "super_secret"
   end
-  
+
   get '/' do
     erb :index
   end
-  
+
   helpers do
 
     def logged_in?
@@ -22,5 +24,4 @@ class ApplicationController < Sinatra::Base
     end
 
   end
-  
 end
