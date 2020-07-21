9bc63d77d76e8f12c75d0f8bba531e648a356c62
Bryce B <bryce.boddy@gmail.com>
2020-07-21 09:43:22 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/tweets'
+    end
+  end
 
 end
