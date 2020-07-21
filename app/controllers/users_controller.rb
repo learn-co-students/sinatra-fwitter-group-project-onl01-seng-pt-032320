947f4247047f61f19a3d7997b3c5a175ac3d9d39
Bryce B <bryce.boddy@gmail.com>
2020-07-21 10:40:40 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect to '/signup'
     else
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
