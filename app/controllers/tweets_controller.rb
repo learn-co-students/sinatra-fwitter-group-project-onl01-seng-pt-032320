ecd707f15e65c957dee92718ef06010ecc046815
Bryce B <bryce.boddy@gmail.com>
2020-07-22 02:33:38 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
-      erb :'tweets/tweets'
+      erb :'tweets_all'
     else
       redirect to '/login'
     end
