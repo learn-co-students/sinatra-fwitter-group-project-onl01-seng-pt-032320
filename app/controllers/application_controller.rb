767a8dfddcaba71ab681a1d382b36360851dece3
Bryce B <bryce.boddy@gmail.com>
2020-07-22 07:05:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,22 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "super_secret"
+    set :session_secret, "fwitter_secret"
   end
 
   get '/' do
     erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
   end
 end
